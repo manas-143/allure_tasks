b32b90feba38f2a1a2e2249a51d99cda2c7a1663
Manas <122993008+manas-143@users.noreply.github.com>
2023-11-10 11:24:38 +0530

    Add files via upload

--- features/environment.py
+++ features/environment.py
@@ -1,23 +1,31 @@
+import shutil
+
+from allure_commons._allure import attach
+from allure_commons.types import AttachmentType
 from playwright.sync_api import sync_playwright
 import os
+
+current_directory = os.getcwd()
 
 
 def before_all(context):
     context.p = sync_playwright().start()
 
-    path = './videos'
-    parent_directory = './'
+    Path = f'{current_directory}\Video'
+    print(Path)
 
-    is_exist = os.path.exists(path)
-    if is_exist == False:
-        path = os.path.join(parent_directory, "videos")
-        os.mkdir(path)
+    is_exist = os.path.exists(Path)
+    print(is_exist)
+    if is_exist:
+        shutil.rmtree(Path)
+    Path = f'{current_directory}\Video'
+    os.mkdir(Path)
 
 
 def before_scenario(context, scenario):
-    context.browser = context.p.chromium.launch(headless=True, slow_mo=5000)
+    context.browser = context.p.chromium.launch(headless=False, slow_mo=5000)
     context.tab = context.browser.new_context(
-        record_video_dir="videos/",
+        record_video_dir="Video/",
         record_video_size={"width": 1500, "height": 1200}
     )
     context.page = context.tab.new_page()
@@ -26,5 +34,15 @@ def before_scenario(context, scenario):
 def after_scenario(context, scenario):
     context.page.close()
     context.page.video.save_as(
-        f"./videos/{scenario.name}.webm"
+        f"{current_directory}/Video/{scenario.name}.webm"
     )
+    context.page.video.save_as(os.path.join(current_directory, f"Video/{scenario.name}"))
+    with open(
+            os.path.join(current_directory, context.page.video.path()), "rb"
+    ) as video_file:
+        # Video
+        attach(
+            video_file.read(),
+            name=f"Video : {scenario.name}",
+            attachment_type=AttachmentType.WEBM,
+        )
--- runner_behave.py
+++ runner_behave.py
@@ -1,43 +1,29 @@
-
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 from behave.__main__ import run_behave
 from behave.configuration import Configuration
-from behave.tag_expression import TagExpression
+
+# PROJECT_ROOT = "C://Users//a98016118//Desktop//amazon//Amazon_POM"
+PROJECT_ROOT = os.getcwd()
 
 
-# from features.environment import PROJECT_ROOT
-# from utils.env import get_from_env
-# from utils.env import is_truthy
-# from utils.env import load_env
-# from utils.logger import log
 def main():
     load_dotenv()
-    my_details = os.getenv("ENVIRONMENT")
-    print(my_details)
-    # tags = TagExpression(get_from_env("TAGS").split())
-    # tags_option = " ".join([f"--tags={','.join(tag)}" for tag in tags.ands])
-    # log.error(f"Using these tags: {tags_option}")
-    #
-    # stop_fail = "--stop" if is_truthy(os.getenv("STOP_FAIL")) else ""
-    # log.info(f'Stop at first fail?: {get_from_env("STOP_FAIL")}')
-    reports = "./" + "/" + os.getenv(
+    reports = PROJECT_ROOT + "/" + os.getenv(
         "REPORTS_FOLDER")
     reports = os.path.normpath(f'"{reports}"')
     feature_order = '" "'.join(
-        os.path.join("./",
+        os.path.join(PROJECT_ROOT,
                      feature_path.strip())
         for feature_path in os.getenv("FEATURE_ORDER").split(",")
         if Path(
-            "./" + "/" + feature_path.strip()).exists()
+           PROJECT_ROOT + "/" + feature_path.strip()).exists()
     )
     feature_order = os.path.normpath(f'"{feature_order}"')
     print(feature_order)
     arguments = (
         f"{feature_order} -f allure_behave.formatter:AllureFormatter -o {reports}"
-        # f"{stop_fail} --no-skipped -f plain {tags_option} --no-capture "
-        # f"--no-capture-stderr --no-logcapture "
     )
     print(arguments)
     # log.error(arguments)
