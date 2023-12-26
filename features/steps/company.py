596c156b75deb9f2388b9bc452291f644b2b41ac
Manas <122993008+manas-143@users.noreply.github.com>
2023-12-26 12:09:34 +0530

    Update company.py

--- features/steps/company.py
+++ features/steps/company.py
@@ -5,15 +5,15 @@ from company_functions import extracting_information_of_company
 
 locators = {
     "INPUT AREA": '//textarea[@id="APjFqb"]',
-    "REVIEWS": '//div[@class="CJQ04"]/div/span[@class="hqzQac"]/span/a',
+    "REVIEWS": "//span[@class='hqzQac']//a//span",
     "ADDRESS": "//div[@data-dtype='d3ifr']/span[@class='LrzXr']",
-    "RATINGS": '//div[@class="CJQ04"]/div/span[@aria-hidden="true"]',
-    "NAME": "//div[@class='SPZz6b']/h2[@data-attrid='title']/span",
-    "DIRECTION BTN": "//a[@class='ab_button' and @tabindex='0']//div"
+    "RATINGS": "//*[@class='Aq14fc']",
+    "NAME": "//div[@data-attrid='title']",
+    "DIRECTION BTN": "(//span[@class='AQtWSd' and @aria-hidden='true'])[2]/following-sibling::div"
 
 }
 
-
+company_details = []
 @given(u'User is on the google website searched for "{places}"')
 def searching_for_places(context, places):
     context.page.goto("https://www.google.co.in/")
@@ -26,8 +26,9 @@ def searching_for_places(context, places):
 def user_extracts_information_of_company(context):
     details = extracting_information_of_company(context.page, locators["NAME"], locators["RATINGS"],
                                                 locators["REVIEWS"], locators["ADDRESS"], locators["DIRECTION BTN"])
-    context.company_details = []  # stores the details of the company
-    context.company_details.append(details)  # details holds a dict containing of information that we append in list
+    print(details)
+    company_details.append(details)  # details holds a dict containing of information that we append in list
+    print(company_details)
 
 
 @then("User makes a file to save the information of company in csv")
@@ -36,10 +37,10 @@ def user_saves_company_details_to_csv(context):
     csv_file_path = 'company_details.csv'
 
     # Extract the keys from the first dictionary in the list
-    field_names = context.company_details[0].keys()
+    field_names = ["Name", "Rating", "Review", "Address", "Latitude and Longitude"]
 
     # Write data to CSV
-    with open(csv_file_path, "a") as csv_file:
+    with open(csv_file_path, mode="w", newline="", encoding='utf-8') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=field_names)
         writer.writeheader()
-        writer.writerows(context.company_details)
+        writer.writerows(company_details)
